daa8d1a8d892ee5cb4a34557adb6bfbfab571f9a
Nate Wiger <nwiger@cpan.org>
2000-10-02 17:50:32 +0000

    Import of NWIGER/Text-Header-1.03 from CPAN.
    gitpan-cpan-distribution: Text-Header
    gitpan-cpan-version:      1.03
    gitpan-cpan-path:         NWIGER/Text-Header-1.03.tar.gz
    gitpan-cpan-author:       NWIGER
    gitpan-cpan-maturity:     released

--- Header.pm
+++ Header.pm
@@ -1,5 +1,5 @@
 
-# $Id: Header.pm,v 1.2 2000/09/28 01:21:51 nwiger Exp $
+# $Id: Header.pm,v 1.3 2000/10/02 17:43:20 nwiger Exp $
 ####################################################################
 #
 # Copyright (c) 2000 Nathan Wiger <nate@sun.com>
@@ -34,8 +34,7 @@ require 5.004;
 
 use strict;
 use vars qw(@EXPORT @ISA $VERSION);
-$VERSION = do { my @r=(q$Revision: 1.2 $=~/\d+/g); 
-                sprintf "%d."."%02d"x$#r,@r };
+$VERSION = do { my @r=(q$Revision: 1.3 $=~/\d+/g); sprintf "%d."."%02d"x$#r,@r };
 
 use Exporter;
 @ISA = qw(Exporter);
@@ -56,7 +55,7 @@ sub header {
         chomp $val;
         push @ret, "$tag: $val\n";
     }
-    return @ret, "\n";
+    return @ret;
 }
 
 sub unheader {
@@ -71,11 +70,11 @@ sub unheader {
         # split the two and change the tag to lowercase 
         my($tag, $val) = $line =~ m/([-\w]+)\s*:\s*(.*)/s;
         $tag = lc $tag;
-        $tag =~ s/-+/_/g;
+        $tag =~ s/-/_/g;
    
         # some cleanup
         $val =~ s/\n\s*/ /g;
-        $val =~ s/\s*,\s*/, /g;
+        $val =~ s/\s*,\s+/, /g;
         push @ret, $tag, $val;
     }
     return @ret;
@@ -87,7 +86,7 @@ __END__
 
 =head1 NAME
 
-Text::Header - provide RFC 822 C<header> and C<unheader> functions
+Text::Header - RFC 822/2068 C<header> and C<unheader> functions
 
 =head1 SYNOPSIS
 
@@ -105,15 +104,14 @@ Text::Header - provide RFC 822 C<header> and C<unheader> functions
    @HEADERS = ("Content-Type: text/html\n",
                "Author: Nathan Wiger\n",
                "Last-Modified: Wed Sep 27 13:31:06 PDT 2000\n",
-               "Accept: text/html, text/plain\n",
-               "\n");
+               "Accept: text/html, text/plain\n");
 
    # Can also construct SMTP headers to format mail
 
    @mail_headers = header(from => 'Nathan Wiger <nate@sun.com>',
                           to => 'perl5-porters@perl.org');
    
-   print $MAIL @mail_headers, "Keep up the great work!\n";
+   print $MAIL @mail_headers, "\nKeep up the great work!\n";
 
    # The above would print this to the $MAIL handle:
 
@@ -124,7 +122,7 @@ Text::Header - provide RFC 822 C<header> and C<unheader> functions
 
 
 =head1 DESCRIPTION
-   
+
 This module provides two new functions, C<header> and C<unheader>,
 which provide general-purpose RFC 822 header construction and parsing.
 They do not provide any intelligent defaults of HTTP-specific methods.
@@ -145,21 +143,37 @@ does the following:
    3. Combines list elements into a comma-delimited
       string 
 
-   4. Appends a singular newline as the last element of
-      the list
+Note that a list is always joined into a comma-delimited string. To
+insert multiple separate headers, simply call C<header> with multiple
+args:
+
+   push @out, header(accept => 'text/html',
+                     accept => 'text/plain');
+
+This would create multiple "Accept:" lines.
 
 Note that unlike C<CGI.pm>, the C<header> function provided here
 does not provide any intelligent defaults. If called as:
 
     @out_headers = header;
 
-It would create a single-element list consisting of "\n". This allows
-C<header> to be more general pupose, so it can provide SMTP and other
-headers as well.
+It will return an empty list. This allows C<header> to be more general
+pupose, so it can provide SMTP and other headers as well. You can also
+use it as a generic text formatting tool, hence the reason it's under
+the C<Text::> hierarchy.
 
 The C<unheader> function works in exactly the opposite direction from
-C<header>, pulling apart headers and returning a list. So, assuming
-the C<@HEADERS> array shown up top:
+C<header>, pulling apart headers and returning a list. C<unheader>:
+
+   1. lc's the entire tag name, converting -'s to _'s
+
+   2. Separates each tag based on the colon delimiter,
+      chomping newlines.
+
+   3. Returns a list of tag/value pairs for easy assignment
+      to a hash
+
+So, assuming the C<@HEADERS> array shown up top:
 
    %myheaders = unheader(@HEADERS);
 
@@ -168,8 +182,8 @@ The hash C<%myheaders> would have the following values:
    %myheaders = (
        content_type => 'text/html',
        author => 'Nathan Wiger',
-       last_modified => 'Wed Sep 27 13:31:06 PDT 2000'
-       accept => 'text/html, text/plain');
+       last_modified => 'Wed Sep 27 13:31:06 PDT 2000',
+       accept => 'text/html, text/plain'
    );
 
 Note that all keys are converted to lowercase, and their values have
@@ -202,7 +216,10 @@ Will result in C<@out> being exactly equivalent to C<@in>.
 =head1 REFERENCES
 
 This is designed as both a Perl 5 module and also a Perl 6 prototype.
-Please see the complete Perl 6 proposal at http://dev.perl.org/rfc/
+Please see the Perl 6 proposal at http://dev.perl.org/rfc/333.html
+
+This module is designed to be fully compliant with the internet
+standards RFC 822 (SMTP Headers) and RFC 2068 (HTTP Headers).
 
 =head1 AUTHOR
 
